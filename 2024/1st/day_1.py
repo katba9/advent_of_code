798904832626d0fc84607d1d1a44e3b541ecd98c
katba9 <katbaggott@icloud.com>
2024-12-03 01:29:06 +0000

    day 1 and 2

---
+++ 2024/1st/day_1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("2024/1st/id.txt", names = ["ids"])
+df[["location1", "location2"]] = df["ids"].str.split("   ", expand=True).apply(pd.to_numeric)
+df = df.sort_values(by="location1", ascending=True).reset_index()
+
+df2 = pd.DataFrame(df["location2"]).sort_values(by="location2", ascending=True).reset_index()
+df2["location1"] = df["location1"]
+df2["difference"] = df2["location1"] - df2["location2"]
+
+total = df2['difference'].abs().sum()
+list = [n * (df2["location2"].value_counts()[n]) for n in df2["location1"] if n in df2["location2"].values]
+
+print(f"\n\33[42m\n\n    * ❆ ₊˚⋆ Part 1: \33[1m{total} ⋆ ₊* ❅ ˚    \n\033[0m\n\n\33[41m\n\n    * ❆ ₊˚⋆ Part 2: \33[1m{sum(list)} ⋆ ₊* ❅ ˚    \n\033[0m\n")
---
+++ 2024/2nd/day_2.py
@@ -0,0 +1,38 @@
+with open("2024/2nd/level.txt", encoding='utf8') as file:
+    data = [line.strip('\n') for line in file.readlines()]
+
+def list_check(list):
+    '''Checks a list solely increases or decreases by no more than 3.'''
+    if all(i > j for i, j in zip(list, list[1:])) == True:
+        if all(j-3 <= i <= j+3 for i, j in zip(list, list[1:])):
+            return True
+    elif all(i < j for i, j in zip(list, list[1:])) == True:
+        if all(j-3 <= i <= j+3 for i, j in zip(list, list[1:])):
+            return True
+    else:
+        return False
+
+def remove_item(list):
+    '''Removes one item from a list and tests if it passes the list_check.'''
+    for i in range(len(list)):
+        new_list = list[:]
+        new_list.pop(i)
+        if list_check(new_list) == True:
+            return True
+
+def count(data):
+    '''Counts number of lists that pass the list_check for part 1 and part 2.'''     
+    part1_count = 0
+    part2_count = 0
+    for level in data:
+        level_list = list(map(int, level.split(" ")))
+        if list_check(level_list) == True:
+            part1_count = part1_count + 1
+            part2_count = part2_count + 1
+        else:
+            if remove_item(level_list) == True:
+                part2_count = part2_count + 1
+    return part1_count, part2_count
+
+count1, count2 = count(data)
+print(f"\n\33[42m\n\n    * ❆ ₊˚⋆ Part 1: \33[1m{count1} ⋆ ₊* ❅ ˚    \n\033[0m\n\n\33[41m\n\n    * ❆ ₊˚⋆ Part 2: \33[1m{count1} ⋆ ₊* ❅ ˚    \n\033[0m\n")
